36e4ae1033e3a02d01110ddd79ea4339ae9f71ae
Dahyun Yu <aydha0110@gmail.com>
2019-03-12 21:01:16 +0900

    [Python] 버블정렬 코드 정리

--- python/sort/bubble_sort.py
+++ python/sort/bubble_sort.py
@@ -1,29 +1,18 @@
+import random
+import time
 
-def bubble_sort1(target):
-    cnt = 0
-    for i in range(len(target)-1):
-        for j in range(len(target)-1):
-            cnt += 1
-            if target[j] > target[j+1]:
-                target[j], target[j+1] = target[j+1], target[j]
-
-    return target, cnt
-
-
-def bubble_sort2(target):
-    cnt = 0
+def bubble_sort(target):
     for i in range(len(target)-1):
         for j in range(len(target)-i-1):
-            cnt += 1
             if target[j] > target[j+1]:
                 target[j], target[j+1] = target[j+1], target[j]
 
-    return target, cnt
+    return target
 
 
 if __name__ == '__main__':
-    target_list = [53, 23, 1, 8, 99, 341, 2, 0, 4]
-    result1, cnt1 = bubble_sort1(target_list)
-    result2, cnt2 = bubble_sort2(target_list)
-    print(result1, cnt1)
-    print(result2, cnt2)
+    target_list = [i for i in range(1000)]
+    target_list = target_list[::-1]
+    start = time.time()
+    result = bubble_sort(target_list)
+    print(time.time() - start)
